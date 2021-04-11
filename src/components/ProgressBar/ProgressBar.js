2de3a963c9c24a351330aafa117d408964093837
Michał Zalewski <1094593+mizalewski@users.noreply.github.com>
2021-04-11 20:32:49 +0200

    Fix progress with 99% in progress bar with overflow

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -10,6 +10,7 @@ const Wrapper = styled.div`
   border-radius: var(--borderRadius);
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  /* trip of corners when progress bar is near full */
   overflow: hidden;
 `
 
